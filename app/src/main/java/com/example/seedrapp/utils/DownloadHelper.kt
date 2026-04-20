28739ce63504d137960dfe7c7e3505e446c32a75
akhiljoseph-spec <akhil.joseph@vvdntech.in>
2026-04-20 11:55:34 +0530

    Update DownloadHelper.kt

--- app/src/main/java/com/example/seedrapp/utils/DownloadHelper.kt
+++ app/src/main/java/com/example/seedrapp/utils/DownloadHelper.kt
@@ -1,11 +1,14 @@
 package com.example.seedrapp.utils
-import android.app.*
-import android.content.*
+
+import android.app.DownloadManager
+import android.content.Context
 import android.net.Uri
+
 object DownloadHelper {
+
     fun download(context: Context, url: String) {
-        val req = DownloadManager.Request(Uri.parse(url))
+        val request = DownloadManager.Request(Uri.parse(url))
         val dm = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        dm.enqueue(req)
+        dm.enqueue(request)
     }
 }
